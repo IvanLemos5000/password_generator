e1c64852c8116adc6eeb83d4d9dfa86bed1a9a38
Ivan Lemos <ivan@focogroup.com>
2019-04-15 16:59:39 -0300

    add functions

--- src/app.py
+++ src/app.py
@@ -5,8 +5,50 @@ upperLetters = string.ascii_uppercase
 lowerLetters = string.ascii_lowercase
 numbers = string.digits
 special = string.punctuation
-password = ''
-n = 0
+
+def weak(lenght):
+    n = 0
+    password = ''
+    while n <= lenght:
+        caracters = list(random.choice(str(numbers)))
+        random.shuffle(caracters)
+        password = ''.join(map(str,caracters)) + password
+        n += 1
+    output(password)
+
+def regular(lenght):
+    n = 0
+    password = ''
+    while n <= lenght:
+        caracters = list(random.choice(str(numbers)+lowerLetters))
+        random.shuffle(caracters)
+        password = ''.join(map(str,caracters)) + password
+        n += 1
+    output(password)
+
+def good(lenght):
+    n = 0
+    password = ''
+    while n <= lenght:
+        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters))
+        random.shuffle(caracters)
+        password = ''.join(map(str,caracters)) + password
+        n += 1
+    output(password)
+
+def strong(lenght):
+    n = 0
+    password = ''
+    while n <= lenght:
+        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters+special))
+        random.shuffle(caracters)
+        password = ''.join(map(str,caracters)) + password
+        n += 1
+    output(password)
+    
+def output(password):
+    print('-' * 60)
+    print('Your Password is:', password)
 
 print("Password's Security Levels:")
 print('1:  4 numbers')
@@ -23,77 +65,35 @@ print('10: 16 numbers + letters + special caracters')
 secure = input('Enter the desired password security level (1 to 10): ')
 
 if (secure == '1'):
-    while n <= 3:
-        caracters = list(random.choice(str(numbers)))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    weak(3)
 
 elif (secure == '2'):
-    while n <= 5:
-        caracters = list(random.choice(str(numbers)))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    weak(5)
         
 elif (secure == '3'):
-    while n <= 7:
-        caracters = list(random.choice(str(numbers)))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    weak(7)
 
 elif (secure == '4'):
-    while n <= 5:
-        caracters = list(random.choice(str(numbers)+lowerLetters))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    regular(5)
         
 elif (secure == '5'):
-    while n <= 7:
-        caracters = list(random.choice(str(numbers)+lowerLetters))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    regular(7)
 
 elif (secure == '6'):
-    while n <= 7:
-        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    good(7)
         
 elif (secure == '7'):
-    while n <= 7:
-        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters+special))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    strong(7)
                 
 elif (secure == '8'):
-    while n <= 9:
-        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters+special))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    strong(9)
                 
 elif (secure == '9'):
-    while n <= 11:
-        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters+special))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1
+    strong(11)
                 
 elif (secure == '10'):
-    while n <= 15:
-        caracters = list(random.choice(str(numbers)+lowerLetters+upperLetters+special))
-        random.shuffle(caracters)
-        password = ''.join(map(str,caracters)) + password
-        n += 1 
+    strong(15)
         
 else:
+    print('-' * 60)
     print('Invalid Option - Try Again!')
-        
-print('-' * 60)
-print('Your Password is:', password)
